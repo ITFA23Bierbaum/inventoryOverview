7d078f53967e9dabb2f9bdf079a77a991582af14
Patric Ziel <zielpatric@gmail.com>
2022-10-18 09:52:47 +0200

    Auftrags-Tabelle umgesetzt

--- inventoryOverview/inventory/urls.py
+++ inventoryOverview/inventory/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.search),
     path('search', views.search, name='order-search'),
-    path('result', views.result, name='order-result'),
-    path('order/<int:id>', views.order, name='order-detail')
+    path('order', views.order, name='order-detail')
 ]
--- inventoryOverview/inventory/views.py
+++ inventoryOverview/inventory/views.py
@@ -7,24 +7,11 @@ from .models import Order, InventoryItem, OrderItem
 # Create your views here.
 def search(request):
     orders = Order.objects.filter()
-    return render(request, 'inventory/search.html', {'title': 'Auftrag suchen', 'orders': orders})
+    return render(request, 'inventory/search.html', {'title': 'Auftrag suchen', 'orders': orders, 'logo': 'ressources/logo.png'})
 
-
-def result(request):
-    if request.method == 'POST':
-        searched = request.POST['searched']
-        if searched != '':
-            items = Order.objects.filter(Q(number__contains=searched))
-            return render(request, 'inventory/result.html', {'searched': searched, 'items': items})
-        else:
-            return render(request, 'inventory/result.html', {'searched': searched})
-    else:
-        return render(request, 'inventory/search.html')
-
-
-def order(request, id):
-    current_order = Order.objects.get(id=id)
+def order(request):
+    orderId= request.POST['selectedOrderId']
+    print(orderId)
+    current_order = Order.objects.get(id=orderId)
     current_items = OrderItem.objects.filter(order=current_order)
-    return render(request, 'inventory/order.html', {'order': current_order, 'items': current_items})
-
-
+    return render(request, 'inventory/order.html', {'order': current_order, 'items': current_items, 'logo': 'ressources/logo.png'})

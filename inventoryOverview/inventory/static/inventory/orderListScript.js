00e69230662f1fa47536ab8d25024d37aefbb605
Patric Ziel <zielpatric@gmail.com>
2022-10-21 06:46:25 +0200

    Oberflächen design

---
+++ inventoryOverview/inventory/static/inventory/orderListScript.js
@@ -0,0 +1,20 @@
+function filter(event) {
+    if (event.keyCode >= 48 && event.keyCode <= 57
+            || event.keyCode >= 65 && event.keyCode <= 90
+            || event.keyCode == 8) {
+        console.log(event.path[0].value);
+        event.
+    }
+}
+
+function addFilterToOrders() {
+    [].slice.call(document.getElementsByClassName('filter')[0].getElementsByTagName('input'))
+            .forEach(function(e) {
+        e.addEventListener('keyup', filter, false);
+    });
+}
+
+function orderClick(orderNo) {
+    document.getElementById('input_redirectValue').value = orderNo;
+    frm_redirect.submit();
+}
--- inventoryOverview/inventory/views.py
+++ inventoryOverview/inventory/views.py
@@ -7,11 +7,11 @@ from .models import Order, InventoryItem, OrderItem
 # Create your views here.
 def search(request):
     orders = Order.objects.filter()
-    return render(request, 'inventory/search.html', {'title': 'Auftrag suchen', 'orders': orders, 'logo': 'ressources/logo.png'})
+    return render(request, 'inventory/orderList.html', {'title': 'Auftrag suchen', 'orders': orders})
 
 def order(request):
     orderId= request.POST['selectedOrderId']
     print(orderId)
     current_order = Order.objects.get(id=orderId)
     current_items = OrderItem.objects.filter(order=current_order)
-    return render(request, 'inventory/order.html', {'order': current_order, 'items': current_items, 'logo': 'ressources/logo.png'})
+    return render(request, 'inventory/order.html', {'order': current_order, 'items': current_items})
